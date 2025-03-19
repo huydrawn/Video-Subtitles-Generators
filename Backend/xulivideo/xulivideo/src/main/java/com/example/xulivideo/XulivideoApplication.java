5102a1a70d02e2c38ff01db65adb7327ec59b4e0
tin2709 <trungtinpham336@gmail.com>
2025-03-19 22:46:51 +0700

    first commit

---
+++ Backend/xulivideo/xulivideo/src/main/java/com/example/xulivideo/XulivideoApplication.java
@@ -0,0 +1,13 @@
+package com.example.xulivideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XulivideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XulivideoApplication.class, args);
+	}
+
+}
---
+++ Backend/xulivideo/xulivideo/src/test/java/com/example/xulivideo/XulivideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xulivideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XulivideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
